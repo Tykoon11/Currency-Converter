99ef7d6f7de99a252a14de4c5bfb9fefb369265d
Tykoon11 <noblemanunachukwu@gmail.com>
2023-01-13 21:41:55 +0100

    server restructure

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ app.use(cors());
 
 app.listen(PORT, () => console.log(`server is running on ${PORT}`));
 
-app.get("/convert", (req, res) => {
-  const BASE_URL = process.env.REACT_APP_BASE_URL;
-  axios
-    .get(BASE_URL)
-    .then((response) => {
-      res.json(response.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+app.get("/convert", async (req, res) => {
+  try {
+    const BASE_URL = process.env.REACT_APP_BASE_URL;
+    axios
+      .get(BASE_URL)
+      .then((response) => {
+        res.send(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } catch (err) {
+    console.log(err);
+    res.send(`Could not fetch data from API`);
+  }
 });
